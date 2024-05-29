762c6619822ec22398dabe220057041e484e71df
Maxime Bellis <maxime.bellis@student.kuleuven.be>
2024-05-29 16:52:52 +0200

    Refactored orders

--- deploy3.sh
+++ deploy3.sh
@@ -10,14 +10,14 @@ mvn clean package
 docker buildx create --use
 
 # Step 3: Build the Docker image
-docker buildx build --platform linux/amd64 -t tristanpelgrims/supplier1 --load .
+docker buildx build --platform linux/amd64 -t tristanpelgrims/supplier3 --load .
 
 # Step 4: Push the Docker image to the registry
-docker push tristanpelgrims/supplier1
+docker push tristanpelgrims/supplier3
 
 # Step 5: SSH into the remote server, stop the existing container, pull the Docker image and run a new container
-SSHPASS='Mazda123456789' sshpass -e ssh azureuser@20.166.251.253 << EOF
-docker stop \$(docker ps -q --filter ancestor=tristanpelgrims/supplier1)
-docker pull tristanpelgrims/supplier1
-docker run -d -p 8080:8080 tristanpelgrims/supplier1
+SSHPASS='azureuser2024!' sshpass -e ssh azureuser@20.54.73.38 << EOF
+docker stop \$(docker ps -q --filter ancestor=tristanpelgrims/supplier3)
+docker pull tristanpelgrims/supplier3
+docker run -d -p 8080:8080 tristanpelgrims/supplier3
 EOF
